af4b557eb491f5ee340e3a8cfa2654bbee4b7811
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2022-11-08 22:49:43 +0530

    Add files via upload

---
+++ git.c
@@ -0,0 +1,57 @@
+/*                     Git Tutorial
+1.  git init :
+        git init initializes the .git folder in the repository.
+    
+
+2.  .git :
+        .git folder stores the history of projects in that repository. It saves the changes made.
+
+
+3.  git status :
+        git status gives the info about file is being uploaded on GitHUb or not after doing some changes in a file. It shows tracked and untracked files.
+
+    
+4.  git add . :
+        git add . will add all untracked files in that repository.
+
+
+5.  git add file.txt :
+        This command will add specific file named as file.txt and not all untracked files.
+
+
+6.  git commit :
+        This command commits all the changes.
+
+
+7.  git commit -m "type your message here" :
+        This command commits all the changes as well as adds some message to it.
+
+
+8.  git log :
+        This command gives info about all previous commits. 
+
+
+9.  git reset 'commit_id' :
+        THis command will delete all the commits made after the commit with given id.
+
+
+10. git stash :
+        If we don't want ot commit changes immediately.
+
+
+11. git stash pop :
+        It will commit all changes that were in stash.
+
+
+12. git stash clear :
+        All the changes in stash willl be deleted.
+
+
+13. git remote add origin 'url of github repository'
+        It attaches the current folder you are in with the github repository in the url.
+
+
+14. git push origin master :
+        It pushes to url 'origin' and branch 'master'.
+
+*/
